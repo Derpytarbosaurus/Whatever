08f5439e2c2bc8ee17f6f002912ed20916aabee3
Daniel <danracer87@gmail.com>
2015-10-03 13:25:41 -0700

    No eating my house

---
+++ build/CMakeFiles/thederp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thederp.dir/haiworld.cpp.o"
+  "thederp.pdb"
+  "thederp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thederp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
